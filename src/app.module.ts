9f8e393d728674083382f8f0a8e322ef72950ecc
Daniel Ferreira <silva_120000@yahoo.com.br>
2024-06-25 05:29:10 -0300

    adding users resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { AppController } from './app.controller'
 import { AppService } from './app.service'
 
 import { EnvConfigModule } from './shared/infrastructure/env-config/env-config.module'
+import { UsersModule } from './users/infrastructure/users.module'
 
 @Module({
-  imports: [EnvConfigModule],
+  imports: [EnvConfigModule, UsersModule],
 
   controllers: [AppController],
 
